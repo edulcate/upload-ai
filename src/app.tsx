3056a1b0c2d5a2b84090f57fd56c135dc8ba48be
edulcate <eduardolecate@gmail.com>
2023-09-20 12:37:21 -0300

    URLs atualizadas para Vercel

--- src/app.tsx
+++ src/app.tsx
@@ -32,7 +32,7 @@ export function App() {
 		completion,
 		isLoading
 	} = useCompletion({
-		api: 'http://localhost:3333/ai/complete',
+		api: 'https://upload-ai-api-pied.vercel.app/ai/complete',
 		body: {
 			videoId,
 			temperature
