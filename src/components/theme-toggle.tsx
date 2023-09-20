5b8d909a29cbc64ef95483d69091dc0453fc7009
edulcate <eduardolecate@gmail.com>
2023-09-20 10:26:43 -0300

    Melhoria no código do componente de switch do tema

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,32 +1,27 @@
 import { Moon, Sun } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { useTheme } from '@/components/theme-provider';
+import { Theme, useTheme } from '@/components/theme-provider';
 
 export const ThemeToggle = () => {
     const { theme, setTheme } = useTheme(); 
+    const toggleToTheme: Theme = (theme === 'dark' ? 'light' : 'dark');
 
     return (
         <>
             <Button
-                onClick={() => setTheme('dark')}
+                onClick={() => setTheme(toggleToTheme)}
                 data-theme={theme}
-                className='data-[theme="dark"]:hidden'
+                className={`data-[theme="${toggleToTheme}"]:hidden`}
                 variant='outline'
-                title='Alternar para tema claro'
+                title={`Alternar para tema ${theme === 'dark' ? 'claro' : 'escuro'}`}
             >
-                <Moon className='w-4 h-4 text-primary' />
-            </Button>
-
-            <Button
-                onClick={() => setTheme('light')}
-                data-theme={theme}
-                className='data-[theme="light"]:hidden'
-                variant='outline'
-                title='Alternar para tema escuro'
-            >
-                <Sun
-                    className='w-4 h-4 text-primary'
-                />
+                { theme === 'light' && (
+                    <Moon className=' w-4 h-4 text-primary' />
+                ) }
+                { theme === 'dark' && (
+                    <Sun className=' w-4 h-4 text-primary' />
+                ) }
+                <span className='text-primary ml-2'>Modo {theme === 'dark' ? 'claro' : 'escuro'}</span>
             </Button>
         </>
     );    
